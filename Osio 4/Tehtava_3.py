4c89b55645b2814c79e11bb4c031fd2f1f4a37d1
jankf <Jan.Falck@metropolia.fi>
2023-01-16 15:50:16 +0200

    Osio 8 aloitettu

--- Osio 4/Tehtava_3.py
+++ Osio 4/Tehtava_3.py
@@ -2,15 +2,15 @@ x = input("Anna luku: ")
 if x == "":
     print("Toiminta lopetettu!")
 else:
-    min = int(x)
-    max = int(x)
+    min = float(x)
+    max = float(x)
 
 
 while x != "":
-    if int(x) < min:
-        min = int(x)
-    elif int(x) > max:
-        max = int(x)
+    if float(x) < min:
+        min = float(x)
+    elif float(x) > max:
+        max = float(x)
     x = input("Anna luku: ")
 
 print(f"Toiminta lopetettu! Pienin annettu luku oli {str(min):3} ja suurin annettu luku oli {str(max):3}.")
--- Osio 8/Tehtävä 1.py
+++ Osio 8/Tehtävä 1.py
@@ -1,23 +1,24 @@
-import mysql.connector
+import mariadb
 
-def haeLentokentta(icao):
-    sql = "SELECT name, municipality FROM airport WHERE ident =" + icao +";"
+
+def haeLentokentta():
+    sql = 'SELECT name, municipality FROM airport WHERE ident ="EFHK"'
     print(sql)
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
-    if kursori.rowcount >0 :
-        for rivi in tulos:
-            print(f"Päivää! Olen {rivi[2]} {rivi[1]}. Palkkani on {rivi[3]} euroa kuussa.")
+    print(tulos)
     return
 
-yhteys = mysql.connector.connect(
-         host='127.0.0.1',
-         port= 3306,
-         database='flight_game',
-         user='pythontest',
-         password='pythonpw',
-         autocommit=True
-         )
 
+yhteys = mariadb.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='pythontest',
+    password='pythonpw',
+    autocommit=True
+)
+
+haeLentokentta()
 
