16e4fe63ba07ebacf5cf370883eb3c4f5017b90e
jankf <Jan.Falck@metropolia.fi>
2023-01-16 13:32:35 +0200

    Osio 7 tehty

--- Osio 7/Tehtava_3.py
+++ Osio 7/Tehtava_3.py
@@ -1,15 +1,25 @@
 print("Tervetuloa lentokenttätietokantaan, voit käyttää tietokantaa seuraavilla komennoilla: \
 'Hae', 'Lisää', 'Lopeta'")
 
+db = {}
+
 cmd = input("Anna komento:")
 
 while cmd != "Lopeta":
     if cmd == "Lisää":
-        print("lisays")
+        # print("lisays")
+        nimi = input("Anna lentokentän nimi: ")
+        koodi = input("Anna lentokentän ICAO-koodi: ")
+        db[koodi] = nimi
     elif cmd == "Hae":
-        print("Haku")
-    elif cmd == "Lopeta":
-        break
+        koodi = input("Anna lentokentän ICAO-koodi: ")
+        print(f"Koodia vastaava lentokenttä on: {db[koodi]}")
+        # print("Haku")
+    # elif cmd == "Lopeta":  # tällä hetkellä taitaa olla turha mut antaa nyt olla
+    #     print("Heippa!")
+    #     break
     else:
         print("Komentoa ei tunnistettu, yritä uudelleen.")
     cmd = input("Anna komento: ")
+    if cmd == "Lopeta":
+        print("Heippa!")
