8b46995837130046c60bcd63e15e282a396a0cbc
grummar <Jan.Falck@metropolia.fi>
2023-01-28 00:10:11 +0200

    osio 12 melkein valmis

--- Osio 12/Tehtava 2.py
+++ Osio 12/Tehtava 2.py
@@ -10,11 +10,14 @@ try:
     if vastaus.status_code == 200:
         print("Toimii!")
         jsonvastaus = vastaus.json()
-        saapyynto = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(jsonvastaus[0]["lat"]) + "&lon=" + str(jsonvastaus[0]["lon"]) + "&appid=55cff1485baee5623423b4e365e15aef"
+        saapyynto = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(jsonvastaus[0]["lat"]) + "&lon=" +\
+                    str(jsonvastaus[0]["lon"]) + "&appid=55cff1485baee5623423b4e365e15aef"
         saa = requests.get(saapyynto)
         if saa.status_code == 200:
             json_saa = saa.json()
-            print(json_saa["weather"][0]["description"])
+            saatyyppi = json_saa["weather"][0]["description"]
+            celsius = float(json_saa["main"]["temp"]) - 273.15
+            print(f"Säätyyppi: {saatyyppi}, lämpötila: {celsius}")
 
 
 
